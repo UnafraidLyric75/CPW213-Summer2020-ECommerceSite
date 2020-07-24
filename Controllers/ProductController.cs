0d87bb73024343053cdc984e531fc8f8e596f276
John Uthus <john.a.uthus.jr1@gmail.com>
2020-07-24 12:50:45 -0700

    Created product listing page

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eCommerceSite.Data;
+using eCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerceSite.Controllers
+{
+    public class ProductController : Controller
+    {
+        private ProductContext _context;
+
+        public ProductController(ProductContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Displays a view that list all products
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            // Get all product from database
+            List<Product> products = _context.Products.ToList();
+            // or
+            //List<Product> products =
+            //    (from p in _context.Products
+            //     select p).ToList();
+
+            // Send list of products to view to be display
+            return View(products);
+        }
+    }
+}
