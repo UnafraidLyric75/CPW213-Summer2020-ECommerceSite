b1620b0d6b952a203a9b8fd2952fd8be22c46583
John Uthus <63675144+UnafraidLyric75@users.noreply.github.com>
2020-07-30 17:45:06 -0700

    Issue#5 add product (#12)
    * fixed add product error
    
    * Styled Product Added succefully message

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -5,12 +5,13 @@ using System.Threading.Tasks;
 using eCommerceSite.Data;
 using eCommerceSite.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace eCommerceSite.Controllers
 {
     public class ProductController : Controller
     {
-        private ProductContext _context;
+        private readonly ProductContext _context;
 
         public ProductController(ProductContext context)
         {
@@ -18,20 +19,75 @@ namespace eCommerceSite.Controllers
         }
 
         /// <summary>
-        /// Displays a view that list all products
+        /// Displays a view that lists all products
         /// </summary>
-        /// <returns></returns>
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            // Get all product from database
-            List<Product> products = _context.Products.ToList();
-            // or
-            //List<Product> products =
-            //    (from p in _context.Products
-            //     select p).ToList();
+            // Get all products from database
+            // List<Product> products = _context.Products.ToList();
+            List<Product> products =
+                await (from p in _context.Products
+                       select p).ToListAsync();
 
-            // Send list of products to view to be display
+
+            // Send list of products to view to be displayed
             return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Product p)
+        {
+            if (ModelState.IsValid)
+            {
+                // Add to DB
+                _context.Products.Add(p);
+                await _context.SaveChangesAsync();
+
+                TempData["Message"] = $"{p.Title} was added successfully";
+
+                // redirect back to catalog page
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            // Get product with corresponding id
+            Product p =
+                await (from prod in _context.Products
+                       where prod.ProductId == id
+                       select prod).SingleAsync();
+
+            //Product p2 = await _context
+            //                .Products
+            //                .Where(prod => prod.ProductId == id)
+            //                .SingleAsync();
+
+            // pass product to view
+            return View(p);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Product p)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Entry(p).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+
+                ViewData["Message"] = "Product updated successfully";
+            }
+
+            return View(p);
         }
     }
 }
