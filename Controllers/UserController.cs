1740796773f79f29e66cd7d43836b6eb67c0d783
John Uthus <63675144+UnafraidLyric75@users.noreply.github.com>
2020-08-25 22:30:53 -0700

    Issue#26 user login (#31)
    * Created login page
    
    * added login error message
    
    * enable sessions
    
    * added note
    
    * Added session managment for login
    
    * Register httpContext service to acess session in layout page
    
    * Added greeting to navagtion if user is logged in
    
    * Added username greeting

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Threading.Tasks;
 using eCommerceSite.Data;
 using eCommerceSite.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace eCommerceSite.Controllers
 {
@@ -45,7 +48,49 @@ namespace eCommerceSite.Controllers
 
             }
 
-            throw new NotImplementedException(); 
+            return View(reg);
+        }
+
+        public IActionResult Login()
+        {
+            // Check if user already logged in
+            if (HttpContext.Session.GetInt32("UserId").HasValue)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            UserAccount account =
+                       await (from u in _context.UserAccounts
+                       where (u.Username == model.UsernameOrEmail
+                          || u.Email == model.UsernameOrEmail)
+                          && u.Password == model.Password
+                       select u).SingleOrDefaultAsync();
+
+            if(account == null)
+            {
+                // credential did not match
+
+                // Custom error msg
+                ModelState.AddModelError(string.Empty, "Credential were not found");
+
+                return View(model);
+            }
+
+            // Log user into website
+            HttpContext.Session.SetInt32("UserId", account.UserId);
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
--- Models/UserAccount.cs
+++ Models/UserAccount.cs
@@ -50,4 +50,15 @@ namespace eCommerceSite.Models
         [DataType(DataType.Date)]
         public DateTime? DateOfBirth { get; set; }
     }
+
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Username or Email")]
+        public string UsernameOrEmail { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
 }
