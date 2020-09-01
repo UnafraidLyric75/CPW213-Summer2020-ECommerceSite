9eeacab2e39d47ac25640a1290488f5469f85a9f
John Uthus <john.a.uthus.jr1@gmail.com>
2020-09-01 12:57:30 -0700

    Able to store mutliplr items

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -32,8 +32,24 @@ namespace eCommerceSite.Controllers
         {
             // Get product from the database
             Product p = await ProductDB.GetProductAsync(_context, id);
+
+            const string CartCookie = "CartCookie";
+
+            // get existing cart items
+            string existingItems = _httpContext.HttpContext.Request.Cookies[CartCookie];
+
+            List<Product> cartProducts = new List<Product>();
+            if(existingItems != null)
+            {
+                cartProducts = JsonConvert.DeserializeObject<List<Product>>(existingItems);
+            }
+
+            // add current product to to existing cart
+            cartProducts.Add(p);
+
             // Add product to cart cookie
-            string data = JsonConvert.SerializeObject(p);
+            string data = JsonConvert.SerializeObject(cartProducts);
+
             CookieOptions options = new CookieOptions()
             {
                 Expires = DateTime.Now.AddYears(1),
@@ -41,7 +57,7 @@ namespace eCommerceSite.Controllers
                 IsEssential = true
             };
 
-            _httpContext.HttpContext.Response.Cookies.Append("CartCookie", data, options);
+            _httpContext.HttpContext.Response.Cookies.Append(CartCookie, data, options);
 
             // redirct back to prevoius page
             return RedirectToAction("Index", "Product");
