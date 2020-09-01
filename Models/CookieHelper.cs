b4b0e1fbf773ebf74746b7905796a6c7c8123c43
John Uthus <63675144+UnafraidLyric75@users.noreply.github.com>
2020-09-01 14:08:08 -0700

    Dislay cart item count in navagation (#39)

--- Models/CookieHelper.cs
+++ Models/CookieHelper.cs
@@ -51,7 +51,8 @@ namespace eCommerceSite.Models
 
         public static int GetTotalCartProducts(IHttpContextAccessor http)
         {
-            throw new NotImplementedException();
+            List<Product> cartProducts = GetCartProducts(http);
+            return cartProducts.Count;
         }
     }
 }
